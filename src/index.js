bf56e3e7e18930ea5778f039994af65ca7dd017f
Skeli <sesyl.schweitzer@gmail.com>
2021-10-19 20:53:40 -0400

    Remove Strict Mode for Heroku

--- src/index.js
+++ src/index.js
@@ -7,9 +7,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <App />,
   document.getElementById('root')
 );
 
