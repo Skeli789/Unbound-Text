70f37612650d9c0ddcdf474ff5130b21bdbf4565
Skeli <17243618+Skeli789@users.noreply.github.com>
2025-04-25 14:30:31 +0300

    Modify only test to make sure page loads

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react';
+import {render} from '@testing-library/react';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+// Mock darkreader before imports because the browser's crypto object is not defined in the test environment
+jest.mock('darkreader', () => ({enable: jest.fn(), disable: jest.fn(), auto: jest.fn(), setFetchMethod: jest.fn(), isEnabled: jest.fn()}));
+
+import App from '../App';
+
+test('renders app', () =>
+{
+    const {getByTestId} = render(<App />);
+    const mainPageDiv = getByTestId('main-page');
+    expect(mainPageDiv).toBeInTheDocument();
 });
